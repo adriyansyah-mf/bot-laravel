234bda84ef62ce1dc066dddac565de73dcd99c1e
AdrianMF <72158292+adriyansyah-mf@users.noreply.github.com>
2021-11-17 14:44:08 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2425,6 +2425,7 @@ class FuckBot:
     def aws_generator(self, length, region):
         
         chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","/","/"]
+        chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
         
         def aws_id():
             output = "AKIA"
